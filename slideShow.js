0a9410448858d75df33324a756b868f758d8e1d4
chenkevin <chenkevin@synology.com>
2016-11-29 18:23:10 +0800

    Change method inside constructor to prototype

--- slideShow.js
+++ slideShow.js
@@ -6,381 +6,377 @@ function Slide(para) {
 	this.items = para.items;
 	this.curImgID = 0;
 	this.timer = undefined;
+	this.navOldPos = [];
+	this.navCurPos = [];
+}
 
-	// Private member
-	var navOldPos = [];
-	var navCurPos = [];
+// Start
+Slide.prototype.start = function() {
+	var display = {};
+	var output = "";
+	var that = this;
 
-	// Start
-	this.start = function() {
-		var display = {};
-		var output = "";
-		var that = this;
-
-		// Get display area
-		display = document.getElementById(this.id);
-
-		// Create image block
-		output += this.createImageBlock();
-
-		// Create button
-		output += this.createButton();
-
-		// Create navbar
-		output += this.createNavbar();
-
-		// Apply content
-		display.className = "slideShow main";
-		display.innerHTML = output;
-
-		// Bind events
-		this.bindEvent();
-	}
+	// Get display area
+	display = document.getElementById(this.id);
 
 	// Create image block
-	this.createImageBlock = function() {
-		var i = 0;
-		var output = "";
-
-		// For each image
-		for (i = 0; i < this.items.length; i++) {
-			output += "<div";
-			output += " id=\"slideImgB" + i + "\"";
-			output += " class=\"slideShow image\"";
-			output += ">";
-
-			// Image
-			output += "<img";
-			output += " src=\"" + this.items[i] + "\"";
-			output += " id=\"slideImg" + i + "\"";
-			output += " class=\"slideShow\"";
-			output += ">";
-
-			output += "</div>";
-		}
-
-		return output;
-	}
+	output += this.createImageBlock();
 
 	// Create button
-	this.createButton = function() {
-		var output = "";
-
-		// Prev button
-		output += "<div";
-		output += " id=\"slideBtnPrev\"";
-		output += " class=\"slideShow button\"";
-		output += " style=\"left: 1%\"";
-		output += ">";
-		output += "<img";
-		output += " src=\"img/left.png\"";
-		output += " style=\"width:100%;height:100%;\"";
-		output += ">";
-		output += "</div>";
-
-		// Next button
-		output += "<div";
-		output += " id=\"slideBtnNext\"";
-		output += " class=\"slideShow button\"";
-		output += " style=\"right: 1%\"";
-		output += ">";
-		output += "<img";
-		output += " src=\"img/right.png\"";
-		output += " style=\"width:100%;height:100%;\"";
-		output += ">";
-		output += "</div>";
-
-		return output;
-	}
+	output += this.createButton();
 
 	// Create navbar
-	this.createNavbar = function() {
-		var i = 0;
-		var output = "";
+	output += this.createNavbar();
 
-		output += "<div";
-		output += " id=\"navbar\"";
-		output += " class=\"slideShow navbar\"";
-		output += ">";
-
-		// Create a list of image box
-		for (i = 0; i < this.items.length; i++) {
-			output += "<div";
-			output += " id=\"navbox" + i + "\"";
-			output += " class=\"slideShow navbox\"";
-			output += ">";
-
-			// Images
-			output += "<img";
-			output += " src=\"" + this.items[i] + "\"";
-			output += " id=\"navimg" + i + "\"";
-			output += " class=\"slideShow\"";
-			output += ">";
-
-			output += "</div>";
-		}
-
-		output += "</div>";
-
-		return output;
-	}
-	
-	// Change image
-	this.changeImage = function(shift) {
-		var i = 0;
-		var curImgB = {};
-		var maxLen = this.items.length;
-		var that = this;
-
-		// Hide current image block
-		curImgB = document.getElementById("slideImgB" + this.curImgID);
-		curImgB.style.display = "none";
-
-		// Update current image ID
-		this.curImgID += shift;
-		if (this.curImgID < 0) this.curImgID += maxLen;
-		if (this.curImgID >= maxLen) this.curImgID -= maxLen;
-
-		// Show current image block
-		curImgB = document.getElementById("slideImgB" + this.curImgID);
-		curImgB.style.display = "block";
-		
-		// Adjust image size
-		this.adjustImageSize(this.curImgID);
-
-		// Adjust navbar position
-		this.adjustNavbarPosition(false);
-
-		// Destroy timer if defined
-		if (this.timer != undefined) {
-			clearTimeout(this.timer);
-
-			// Start timer again
-			this.timer = window.setTimeout(function(){that.changeImage(+1);}, 3000);
-		}
-	}
+	// Apply content
+	display.className = "slideShow main";
+	display.innerHTML = output;
 
 	// Bind events
-	this.bindEvent = function() {
-		var that = this;
-		var i = 0;
-		var obj;
+	this.bindEvent();
+}
 
-		// Bind onload event for the whole document
-		window.onload = function() {
+// Create image block
+Slide.prototype.createImageBlock = function() {
+	var i = 0;
+	var output = "";
 
-			// Adjust navbar size
-			that.adjustNavbarSize();
+	// For each image
+	for (i = 0; i < this.items.length; i++) {
+		output += "<div";
+		output += " id=\"slideImgB" + i + "\"";
+		output += " class=\"slideShow image\"";
+		output += ">";
 
-			// Turn on and adjust image size
-			that.changeImage(0);
+		// Image
+		output += "<img";
+		output += " src=\"" + this.items[i] + "\"";
+		output += " id=\"slideImg" + i + "\"";
+		output += " class=\"slideShow\"";
+		output += ">";
 
-			// Start first timer
-			that.timer = window.setTimeout(function(){that.changeImage(+1);}, 3000);
-		}
-
-		// Bind onresize event for window
-		window.onresize = function() {
-			that.adjustImageSize(that.curImgID);
-			that.adjustNavbarSize();
-			that.adjustNavbarPosition(true);
-		}
-
-		// Bind onlick event for prev/next buttons
-		document.getElementById("slideBtnPrev").onclick = function() {
-			that.changeImage(-1);
-		}
-		document.getElementById("slideBtnNext").onclick = function() {
-			that.changeImage(+1);
-		}
-
-		// For each navbox
-		for (i = 0; i < this.items.length; i++) {
-			obj = document.getElementById("navbox" + i);
-
-			// onclick
-			obj.onclick = function(id) {
-				return function() {
-					that.changeImage(id - that.curImgID);
-				}
-			}(i);
-
-			// Hover effect
-			obj.onmouseover = function() {
-				this.style.opacity = "1";
-			}
-			obj.onmouseout = function(id) {
-				return function() {
-					if (id != that.curImgID) {
-						this.style.opacity = "0.3";
-					}
-				}
-			}(i);
-		}
+		output += "</div>";
 	}
+	return output;
+}
+
+// Create button
+Slide.prototype.createButton = function() {
+	var output = "";
+
+	// Prev button
+	output += "<div";
+	output += " id=\"slideBtnPrev\"";
+	output += " class=\"slideShow button\"";
+	output += " style=\"left: 1%\"";
+	output += ">";
+	output += "<img";
+	output += " src=\"img/left.png\"";
+	output += " style=\"width:100%;height:100%;\"";
+	output += ">";
+	output += "</div>";
+
+	// Next button
+	output += "<div";
+	output += " id=\"slideBtnNext\"";
+	output += " class=\"slideShow button\"";
+	output += " style=\"right: 1%\"";
+	output += ">";
+	output += "<img";
+	output += " src=\"img/right.png\"";
+	output += " style=\"width:100%;height:100%;\"";
+	output += ">";
+	output += "</div>";
+
+	return output;
+}
+
+// Create navbar
+Slide.prototype.createNavbar = function() {
+	var i = 0;
+	var output = "";
+
+	output += "<div";
+	output += " id=\"navbar\"";
+	output += " class=\"slideShow navbar\"";
+	output += ">";
+
+	// Create a list of image box
+	for (i = 0; i < this.items.length; i++) {
+		output += "<div";
+		output += " id=\"navbox" + i + "\"";
+		output += " class=\"slideShow navbox\"";
+		output += ">";
+
+		// Images
+		output += "<img";
+		output += " src=\"" + this.items[i] + "\"";
+		output += " id=\"navimg" + i + "\"";
+		output += " class=\"slideShow\"";
+		output += ">";
+
+		output += "</div>";
+	}
+	output += "</div>";
+
+	return output;
+}
+
+// Change image
+Slide.prototype.changeImage = function(shift) {
+	var i = 0;
+	var curImgB = {};
+	var maxLen = this.items.length;
+	var that = this;
+
+	// Hide current image block
+	curImgB = document.getElementById("slideImgB" + this.curImgID);
+	curImgB.style.display = "none";
+
+	// Update current image ID
+	this.curImgID += shift;
+	if (this.curImgID < 0) this.curImgID += maxLen;
+	if (this.curImgID >= maxLen) this.curImgID -= maxLen;
+
+	// Show current image block
+	curImgB = document.getElementById("slideImgB" + this.curImgID);
+	curImgB.style.display = "block";
 
 	// Adjust image size
-	this.adjustImageSize = function(imgID) {
-		var imgBlk = {};
-		var imgSrc = {};
-
-		// Image block and image source
-		imgBlk = document.getElementById("slideImgB" + imgID);
-		imgSrc = document.getElementById("slideImg" + imgID);
-
-		// Adjust image source to fit image block
-		this.adjustToParent(imgSrc, imgBlk);
-	}
-
-	// Adjust navbar size
-	this.adjustNavbarSize = function() {
-		var i = 0;
-		var navImg = {};
-		var navBox = {};
-
-		// Adjust navbox size
-		for (i = 0; i < this.items.length; i++) {
-			navBox = document.getElementById("navbox" + i);
-			navBox.style.width = navBox.clientHeight + "px";
-
-			// Adjust image to fit navbox
-			navImg = document.getElementById("navimg" + i);
-			this.adjustToParent(navImg, navBox);
-		}
-	}
+	this.adjustImageSize(this.curImgID);
 
 	// Adjust navbar position
-	this.adjustNavbarPosition = function(isDirect) {
-		var i = 0;
-		var navbar = {};
-		var navbox = {};
-		var barW = 0, barH = 0, boxS = 0, gap = 0;
-		var leftCnt = 0, shift = 0, mid;
-		var output = "";
-		var that = this;
+	this.adjustNavbarPosition(false);
 
-		// Get size of navbar
-		navbar = document.getElementById("navbar");
-		barW = navbar.clientWidth;
-		barH = navbar.clientHeight;
+	// Destroy timer if defined
+	if (this.timer != undefined) {
+		clearTimeout(this.timer);
 
-		// Get size of navbox (width == height)
-		navbox = document.getElementById("navbox" + this.curImgID);
-		boxS = navbox.clientWidth;
+		// Start timer again
+		this.timer = window.setTimeout(function(){that.changeImage(+1);}, 3000);
+	}
+}
 
-		// Gap between navboxes
-		gap = barH - boxS;
+// Bind events
+Slide.prototype.bindEvent = function() {
+	var that = this;
+	var i = 0;
+	var obj;
 
-		// Calculate count of navboxes on the left/right side
-		leftCnt = parseInt((this.items.length - 1) / 2);
+	// Bind onload event for the whole document
+	window.onload = function() {
 
-		// Clone current positions to old positions
-		navOldPos = navCurPos.slice();
+		// Adjust navbar size
+		that.adjustNavbarSize();
 
-		// Calculate new positions
-		mid = (barW - boxS) / 2;
-		for (i = 0; i < leftCnt; i++) {
-			curID = (this.curImgID - leftCnt + i + this.items.length) % this.items.length;
-			shift = mid - (leftCnt - i) * (boxS + gap);
-			navCurPos[curID] = shift;
+		// Turn on and adjust image size
+		that.changeImage(0);
+
+		// Start first timer
+		that.timer = window.setTimeout(function(){that.changeImage(+1);}, 3000);
+	}
+
+	// Bind onresize event for window
+	window.onresize = function() {
+		that.adjustImageSize(that.curImgID);
+		that.adjustNavbarSize();
+		that.adjustNavbarPosition(true);
+	}
+
+	// Bind onlick event for prev/next buttons
+	document.getElementById("slideBtnPrev").onclick = function() {
+		that.changeImage(-1);
+	}
+	document.getElementById("slideBtnNext").onclick = function() {
+		that.changeImage(+1);
+	}
+
+	// For each navbox
+	for (i = 0; i < this.items.length; i++) {
+		obj = document.getElementById("navbox" + i);
+
+		// onclick
+		obj.onclick = function(id) {
+			return function() {
+				that.changeImage(id - that.curImgID);
+			}
+		}(i);
+
+		// Hover effect
+		obj.onmouseover = function() {
+			this.style.opacity = "1";
 		}
-		for (i = 0; i < this.items.length - leftCnt; i++) {
-			curID = (this.curImgID + i) % this.items.length;
-			shift = mid + i * (boxS + gap);
-			navCurPos[curID] = shift;
-		}
+		obj.onmouseout = function(id) {
+			return function() {
+				if (id != that.curImgID) {
+					this.style.opacity = "0.3";
+				}
+			}
+		}(i);
+	}
+}
 
-		// Apply to navboxes
-		shift = navCurPos[this.curImgID] - navOldPos[this.curImgID];
-		for (i = 0; i < this.items.length; i++) {
-			navbox = document.getElementById("navbox" + i);
-			navbox.style.opacity = ((i == this.curImgID) ? "1" : "0.3");
+// Adjust image size
+Slide.prototype.adjustImageSize = function(imgID) {
+	var imgBlk = {};
+	var imgSrc = {};
 
-			// First time or specified, update positions directly
-			if (navOldPos[i] == undefined || isDirect) {
-				navbox.style.left = navCurPos[i] + "px";
-			} else {
+	// Image block and image source
+	imgBlk = document.getElementById("slideImgB" + imgID);
+	imgSrc = document.getElementById("slideImg" + imgID);
 
-				// Animation for middle boxes
-				if ((navCurPos[this.curImgID] - navOldPos[this.curImgID]) * 
-					(navCurPos[i] - navOldPos[i]) >= 0) {
-					this.shiftAnimation(navbox, navOldPos[i], navCurPos[i], 300);
+	// Adjust image source to fit image block
+	this.adjustToParent(imgSrc, imgBlk);
+}
+
+// Adjust navbar size
+Slide.prototype.adjustNavbarSize = function() {
+	var i = 0;
+	var navImg = {};
+	var navBox = {};
+
+	// Adjust navbox size
+	for (i = 0; i < this.items.length; i++) {
+		navBox = document.getElementById("navbox" + i);
+		navBox.style.width = navBox.clientHeight + "px";
+
+		// Adjust image to fit navbox
+		navImg = document.getElementById("navimg" + i);
+		this.adjustToParent(navImg, navBox);
+	}
+}
+
+// Adjust navbar position
+Slide.prototype.adjustNavbarPosition = function(isDirect) {
+	var i = 0;
+	var navbar = {};
+	var navbox = {};
+	var barW = 0, barH = 0, boxS = 0, gap = 0;
+	var leftCnt = 0, shift = 0, mid;
+	var output = "";
+	var that = this;
+
+	// Get size of navbar
+	navbar = document.getElementById("navbar");
+	barW = navbar.clientWidth;
+	barH = navbar.clientHeight;
+
+	// Get size of navbox (width == height)
+	navbox = document.getElementById("navbox" + this.curImgID);
+	boxS = navbox.clientWidth;
+
+	// Gap between navboxes
+	gap = barH - boxS;
+
+	// Calculate count of navboxes on the left/right side
+	leftCnt = parseInt((this.items.length - 1) / 2);
+
+	// Clone current positions to old positions
+	this.navOldPos = this.navCurPos.slice();
+
+	// Calculate new positions
+	mid = (barW - boxS) / 2;
+	for (i = 0; i < leftCnt; i++) {
+		curID = (this.curImgID - leftCnt + i + this.items.length) % this.items.length;
+		shift = mid - (leftCnt - i) * (boxS + gap);
+		this.navCurPos[curID] = shift;
+	}
+	for (i = 0; i < this.items.length - leftCnt; i++) {
+		curID = (this.curImgID + i) % this.items.length;
+		shift = mid + i * (boxS + gap);
+		this.navCurPos[curID] = shift;
+	}
+
+	// Apply to navboxes
+	shift = this.navCurPos[this.curImgID] - this.navOldPos[this.curImgID];
+	for (i = 0; i < this.items.length; i++) {
+		navbox = document.getElementById("navbox" + i);
+		navbox.style.opacity = ((i == this.curImgID) ? "1" : "0.3");
+
+		// First time or specified, update positions directly
+		if (this.navOldPos[i] == undefined || isDirect) {
+			navbox.style.left = this.navCurPos[i] + "px";
+		} else {
+
+			// Animation for middle boxes
+			if ((this.navCurPos[this.curImgID] - this.navOldPos[this.curImgID]) * 
+					(this.navCurPos[i] - this.navOldPos[i]) >= 0) {
+				this.shiftAnimation(navbox, this.navOldPos[i], this.navCurPos[i], 300);
 
 				// Animation for endpoint boxes
-				} else {
+			} else {
 
-					// Shift left: go left and appear at right
-					// Shift right: go right and appear at left
-					// Hint: no matter which direction, it's the same way as middle navbox
-					this.shiftAnimation(navbox, navOldPos[i], navOldPos[i] + shift, 150, function(obj, id) {
-						return function() {
-							that.shiftAnimation(obj, navCurPos[id] - shift, navCurPos[id], 150);
-						}
-					}(navbox, i));
-				}
+				// Shift left: go left and appear at right
+				// Shift right: go right and appear at left
+				// Hint: no matter which direction, it's the same way as middle navbox
+				this.shiftAnimation(navbox, this.navOldPos[i], this.navOldPos[i] + shift, 150, function(obj, id) {
+					return function() {
+						that.shiftAnimation(obj, that.navCurPos[id] - shift, that.navCurPos[id], 150);
+					}
+				}(navbox, i));
 			}
 		}
 	}
+}
 
-	// Adjust child element to fit parent element
-	this.adjustToParent = function(chd, par) {
-		var pW = 0, pH = 0, cW = 0, cH = 0;
-		var gap = 0;
+// Adjust child element to fit parent element
+Slide.prototype.adjustToParent = function(chd, par) {
+	var pW = 0, pH = 0, cW = 0, cH = 0;
+	var gap = 0;
 
-		// Retrieve width and height
-		pW = par.clientWidth;
-		pH = par.clientHeight;
-		cW = chd.clientWidth;
+	// Retrieve width and height
+	pW = par.clientWidth;
+	pH = par.clientHeight;
+	cW = chd.clientWidth;
+	cH = chd.clientHeight;
+
+	// Dominated by width
+	if (pW/pH < cW/cH) {
+		chd.style.width = pW + "px";
+		chd.style.height = "auto";
+
+		// fix vertical center
 		cH = chd.clientHeight;
-
-		// Dominated by width
-		if (pW/pH < cW/cH) {
-			chd.style.width = pW + "px";
-			chd.style.height = "auto";
-
-			// fix vertical center
-			cH = chd.clientHeight;
-			gap = (pH - cH) / 2.0;
-			chd.style.top = gap + "px";
+		gap = (pH - cH) / 2.0;
+		chd.style.top = gap + "px";
 
 		// Dominated by height
-		} else {
-			chd.style.height = pH + "px";
-			chd.style.width = "auto";
+	} else {
+		chd.style.height = pH + "px";
+		chd.style.width = "auto";
 
-			// Reset top gap
-			chd.style.top = "0px";
-		}
+		// Reset top gap
+		chd.style.top = "0px";
 	}
+}
 
-	// Animation of changing positions with shifting
-	this.shiftAnimation = function(obj, oldPos, newPos, msec, func) {
-		var curPos = 0, shift = 0;
-		var timeID = undefined;
+// Animation of changing positions with shifting
+Slide.prototype.shiftAnimation = function(obj, oldPos, newPos, msec, func) {
+	var curPos = 0, shift = 0;
+	var timeID = undefined;
 
-		// Create animation
-		curPos = oldPos;
-		shift = (newPos - oldPos) / (msec / 20);
-		timeID = setInterval(animationHelp, 20);
+	// Create animation
+	curPos = oldPos;
+	shift = (newPos - oldPos) / (msec / 20);
+	timeID = setInterval(animationHelp, 20);
 
-		// Animation help
-		function animationHelp() {
-			
-			// Reach new position
-			if (curPos == newPos) {
-				clearInterval(timeID);
+	// Animation help
+	function animationHelp() {
 
-				// Call function if specified
-				if (func != undefined) {
-					func();
-				}
-			} else {
-				curPos += shift;
-				if ((curPos - oldPos) * (curPos - newPos) >= 0) {
-					curPos = newPos;
-				}
-				obj.style.left = curPos + "px";
+		// Reach new position
+		if (curPos == newPos) {
+			clearInterval(timeID);
+
+			// Call function if specified
+			if (func != undefined) {
+				func();
 			}
+		} else {
+			curPos += shift;
+			if ((curPos - oldPos) * (curPos - newPos) >= 0) {
+				curPos = newPos;
+			}
+			obj.style.left = curPos + "px";
 		}
 	}
 }
